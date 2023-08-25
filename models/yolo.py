bbf7185c0d41369a82c06e4169a0455c63753881
yuqiuya <141293014+yuqiuya@users.noreply.github.com>
2023-08-25 10:02:59 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -340,6 +340,8 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             c2 = ch[f] * args[0] ** 2
         elif m is Expand:
             c2 = ch[f] // args[0] ** 2
+        elif m is resnet501 or m is resnet502 or m is resnet503:
+	        c2 = args[0]
         else:
             c2 = ch[f]
 
